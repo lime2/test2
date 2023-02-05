dcaede57c86ce39e9cfc75191ee6fd5e3a411b74
lime2 <lime2@github.com>
2023-02-05 10:29:36 -0800

    fixed quiz

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path')
 
 app.use(express.static(path.join(__dirname,'public')))
 
-app.get('/', function (req, res, next) {
+app.get('/', function (req, res) {
     var options = {
       root: path.join(__dirname, 'public'),
       dotfiles: 'deny',
@@ -15,6 +15,21 @@ app.get('/', function (req, res, next) {
     }
     console.log(__dirname)
     res.sendFile('index.html', options)
+})
+
+app.get('/results',function(req,res) {
+  var answer = req.originalUrl
+  var results = {};
+  answer = answer.substring(9)
+  answer = answer.split('&')
+  for (var a of answer) {
+    results[a.split('=')[0]] = a.split('=')[1]
+  }
+  if (results.bestnum == 42 && results.site == 'www.42.org' && results.eat == 'yes' && results.horse == 'bean' && results.like == 5) {
+    res.send('good ye')
+  } else {
+    res.send('nah')
+  }
 })
 
 var port = process.env.PORT || 3000
